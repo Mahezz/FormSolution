bc767125bb5e9550b862026d0170408fb55775e3
angel torres <angeltorres98@hotmail.com>
2022-10-05 10:35:01 +0200

    Add project files.

---
+++ FormSolution/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using FormSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<SelectItem> lst = null;
+
+            using (Models.DatabaseEntities1 db = new Models.DatabaseEntities1())
+            {
+                lst = (from d in db.Country
+                                     select new SelectItem
+                                     {
+                                         Id = d.Id,
+                                         CountryName = d.Name
+                                     }).ToList();
+            }
+
+            List<SelectListItem> items = lst.ConvertAll(
+                d =>
+                {
+                    return new SelectListItem()
+                    {
+                        Text = d.CountryName.ToString(),
+                        Value = d.CountryName.ToString(),
+                        Selected = false
+                    };
+                });
+
+            ViewBag.items = items;
+
+            return View();
+        }
+    }
+}
